e03641b936c3a574a01ed228cc350f768ad757f0
seojiwon0702 <seojiwon335@gmail.com>
2025-08-08 01:57:23 +0900

    25.08.08  streamlit.sh 수정

--- streamlit.sh
+++ streamlit.sh
@@ -1,3 +1,3 @@
 pip install streamlit openai psycopg2-binary python-dotenv langchain-community langchain-openai
 
-python -m streamlit run ./~~~.py --server.port 8000 --server.address 0.0.0.0
+python -m streamlit run ./genius_chat.py --server.port 8000 --server.address 0.0.0.0
